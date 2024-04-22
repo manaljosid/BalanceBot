e68b7199c8f0411706cf9b84624a902fc3fdc41d
manaljosid <mam90@hi.is>
2024-04-22 01:57:06 +0000

    Change to drive a "torque vectored" RC robot

--- Balancebot/main.cpp
+++ Balancebot/main.cpp
@@ -21,42 +21,30 @@
 #include <hardware/gpio.h>
 #include <ELRS.h>
 #include <Servo.h>
-#include <BNO055.h>
-#include <PID.h>
 
 // Defines
 #define THROTTLE_CHANNEL ch0
-#define TURNING_CHANNEL ch1
+#define TURNING_CHANNEL ch3
 #define ARMING_CHANNEL ch5
 
-#define PID_KP 1.0f
-#define PID_KI 5.0f
+#define MAX_THROTTLE 0.3f;
+#define MAX_TURNING_THROTTLE 0.1f;
 
-#define ROLL_VARIABLE angle_y
+#define ARM_DISARM_RAMP_RATE 0.001f;
 
 // Pinouts
 const uint8_t pin_left_motor_pwm = 19;
 const uint8_t pin_right_motor_pwm = 8;
 
-const uint8_t pin_left_motor_dir = 18;
-const uint8_t pin_right_motor_dir = 7;
-
 const uint8_t pin_elrs_tx = 0;
 const uint8_t pin_elrs_rx = 1;
-
-const uint8_t pin_scl = 3;
-const uint8_t pin_sda = 2;
 
 // Constructors
 ELRS* ELRS::interrupt_instance = nullptr; // Necessary for the ELRS interrupts to work
 ELRS receiver;
-BNO055 bno055(i2c1, BNO055_ADDRESS_A);
-PID pid(PID_KP, PID_KI, 0.0f);
 
 Servo left_motor(pin_left_motor_pwm);
 Servo right_motor(pin_right_motor_pwm);
-Servo left_motor_dir(pin_left_motor_dir);
-Servo right_motor_dir(pin_right_motor_dir);
 
 // Global variables and data structures
 queue_t rc_queue;
@@ -66,10 +54,12 @@ elrs_channels_t rc_channels;
 
 bool armed = false;
 bool last_armed = false;
+bool arm_ramping = false;
+bool disarm_ramping = false;
 
 uint16_t signal_loss_timeout = 0;
-float pid_output, left_motor_throttle, right_motor_throttle;
-float angle_x, angle_y, angle_z;
+float throttle_command, left_motor_throttle, right_motor_throttle;
+float arming_ramp = 0.0f;
 
 // Forward declarations and utility functions
 void print_raw_packet(raw_packet_t packet);
@@ -110,34 +100,14 @@ void core_0_init() {
 
     left_motor.init();
     right_motor.init();
-    left_motor_dir.init();
-    right_motor_dir.init();
-
-    busy_wait_ms(3000);
+    busy_wait_ms(500);
 
     // Run ESC initialization sequence
-    for (unsigned int i = 10; i > 0; i--) {
-        left_motor_dir.set_pulse_width(1400 + 10 * i);
-        right_motor_dir.set_pulse_width(1400 + 10 * i);
-        busy_wait_ms(50);
-    }
     left_motor.set_pulse_width(2000);
     right_motor.set_pulse_width(2000);
-
     busy_wait_ms(3500);
-
     left_motor.set_pulse_width(1000);
     right_motor.set_pulse_width(1000);
-
-    bno055.init(pin_sda, pin_scl);
-    bno055.set_ext_crystal_use(true);
-
-    pid.antiwindup = 1.0f / PID_KI;
-    pid.enableAntiwindup = true;
-    pid.errorMode = ErrorMode::LINEAR;
-    pid.derivativeMode = DerivativeMode::DERIVATIVE_ON_ERROR;
-    pid.setpoint = 0.0f;
-
     // Allow the ESCs to actually enable before continuing
     busy_wait_ms(5000);
 
@@ -148,20 +118,14 @@ void core_0_init() {
 }
 
 void core_0_loop() {
-    uint32_t ch0 = (((uint32_t)rc_channels.THROTTLE_CHANNEL) * 1024UL / 1639UL) + 881UL;
-    uint32_t ch1 = (((uint32_t)rc_channels.TURNING_CHANNEL) * 1024UL / 1639UL) + 881UL;
-    printf("%u, %u, %u, %f, %f, ", ch0, ch1, rc_channels.ARMING_CHANNEL, angle_y, pid_output);
-    if (armed) printf("ARMED\n");
-    else printf("NOT ARMED\n");
+    uint32_t ch0 = (uint32_t)rc_channels.THROTTLE_CHANNEL; //(((uint32_t)rc_channels.THROTTLE_CHANNEL) * 1024UL / 1639UL) + 881UL;
+    uint32_t ch1 = (uint32_t)rc_channels.TURNING_CHANNEL; //(((uint32_t)rc_channels.TURNING_CHANNEL) * 1024UL / 1639UL) + 881UL;
+    printf("%u, %u, %u, ", ch0, ch1, rc_channels.ARMING_CHANNEL);
+    if (armed) printf("ARMED, ");
+    else printf("NOT ARMED, ");
+    printf("%f, %f, %f, %f\n", throttle_command, left_motor_throttle, right_motor_throttle, arming_ramp);
     busy_wait_ms(20);
 }
-/*
-FROM THE LEFT MOTOR:
-Clockwise is positive roation on y axis
-Positive on left motor is counter clockwise robot rotation
-
-Positive on left motor is clockwise from the motor's perspective
-*/
 
 bool repeating_timer_callback(struct repeating_timer* t) {
     if (queue_is_empty(&rc_queue)) signal_loss_timeout++;
@@ -170,46 +134,76 @@ bool repeating_timer_callback(struct repeating_timer* t) {
     while(!queue_is_empty(&rc_queue)) {
         queue_try_remove(&rc_queue, &rc_channels);
     }
+    
+    // RC value ranges
+    // 0%   = 174
+    // 50%  = 992
+    // 100% = 1811
 
-    bno055.read(&angle_x, &angle_y, &angle_z);
+    left_motor_throttle = 0.0f;
+    right_motor_throttle = 0.0f;
 
-    // Run PID control
-    pid_output = pid.update(ROLL_VARIABLE / 180.0f, 0.001f);
+    // Notice there's a deadband between 985 and 995
+    if (rc_channels.TURNING_CHANNEL > 997) {
+        // Turn right
+        left_motor_throttle += ((((float)rc_channels.TURNING_CHANNEL) - 992.0f) / 819.0f) * MAX_TURNING_THROTTLE;
+    } else if (rc_channels.TURNING_CHANNEL < 987) {
+        // Turn left
+        right_motor_throttle -= ((((float)rc_channels.TURNING_CHANNEL) - 992.0f) / 819.0f) * MAX_TURNING_THROTTLE;
+    }
 
-    // Make sure the throttle command is positive between 0 and 1
-    bool direction = sign(pid_output);
-    pid_output = direction ? pid_output : -pid_output;
-    pid_output = constrain(pid_output, 0.0f, 1.0f);
+    throttle_command = ((((float)rc_channels.THROTTLE_CHANNEL) - 174.0f) / 1637.0f) * MAX_THROTTLE;
+    left_motor_throttle += throttle_command;
+    right_motor_throttle += throttle_command;
 
     // Arming logic
     armed = rc_channels.ARMING_CHANNEL >= 900;
-    if (!armed) {
-        pid_output = left_motor_throttle = right_motor_throttle = 0.0f;
-    } else if (armed & !last_armed) {
-        pid.reset();
-    }
-    last_armed = armed;
-
-    left_motor_throttle = pid_output;
-    right_motor_throttle = pid_output;
 
     // If 100 ms have passed since last RC signal cut the motors
     if (signal_loss_timeout > 100) {
-        // Cut off motors, set counter compare level to 0 - will make ESCs complain
-        left_motor.set_raw_level(0);
-        right_motor.set_raw_level(0);
-        left_motor_dir.set_raw_level(0);
-        right_motor_dir.set_raw_level(0);
-    } else {
-        // Set motor throttle values
-        // 0 = 190
-        // 1 = 1791
-        //pid_output = (((float)rc_channels.ch0) - 190.0f) / 1601.0f;
-        left_motor.set_throttle(left_motor_throttle);
-        right_motor.set_throttle(right_motor_throttle);
-        left_motor_dir.set_pulse_width(direction ? 1600 : 1400);
-        right_motor_dir.set_pulse_width(direction ? 1600 : 1400);
+        armed = false;
     }
+
+    if (armed & !last_armed) {
+        // Entering armed state
+        arm_ramping = true;
+        arming_ramp = 0.0f;
+    } else if (armed & last_armed) {
+        // In armed state
+        if (arm_ramping) {
+            if (arming_ramp < 1.0f) {
+                left_motor_throttle *= arming_ramp;
+                right_motor_throttle *= arming_ramp;
+                arming_ramp += ARM_DISARM_RAMP_RATE;
+            } else {
+                arm_ramping = false;
+                arming_ramp = 0.0f;
+            }
+        }
+    } else if (!armed & last_armed) {
+        // Exiting armed state
+        disarm_ramping = true;
+        arming_ramp = 1.0f;
+    } else {
+        // Not in armed state
+        if (disarm_ramping) {
+            if (arming_ramp > 0.0f) {
+                left_motor_throttle *= arming_ramp;
+                right_motor_throttle *= arming_ramp;
+                arming_ramp -= ARM_DISARM_RAMP_RATE;
+            } else {
+                disarm_ramping = false;
+                arming_ramp = 0.0f;
+            }
+        } else {
+            left_motor_throttle = 0.0f;
+            right_motor_throttle = 0.0f;
+        }
+    }
+    last_armed = armed;
+    
+    left_motor.set_throttle(left_motor_throttle);
+    right_motor.set_throttle(right_motor_throttle);
     return true;
 }
 
